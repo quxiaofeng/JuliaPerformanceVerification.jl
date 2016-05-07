362eb36dc442002269b11282703b21bb5fcaf54f
Xiaofeng QU <xiaofeng.qu.hk@ieee.org>
2016-05-07 18:53:52 +0800

    Init with a vector add test
    Signed-off-by: Xiaofeng QU <xiaofeng.qu.hk@ieee.org>

--- src/JuliaPerformanceVerification.jl
+++ src/JuliaPerformanceVerification.jl
@@ -2,4 +2,18 @@ module JuliaPerformanceVerification
 
 # package code goes here
 
+export vecaddbyvec, vecaddbyiter
+
+function vecaddbyvec(a, b, c, N)
+	for i in 1:N
+		c = a + b
+	end
+end
+
+function vecaddbyiter(a, b, c, N)
+	for i in 1:N, j in 1:length(a)
+		c[j] = a[j] + b[j]
+	end
+end
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,3 +3,26 @@ using Base.Test
 
 # write your own tests here
 @test 1 == 1
+
+
+iteration_cnt_range = [ 10^i for i in 1:10 ]
+vector_size_range   = [ 10^i for i in 1:10 ]
+vector_wins = trues(length(vector_size_range), length(iteration_cnt_range))
+for (i, current_vector_size) in enumerate(vector_size_range), (j, iteration_cnt) in enumerate(iteration_cnt_range)
+	a = rand(current_vector_size)
+	b = rand(current_vector_size)
+	c = zeros(current_vector_size)
+	tic()
+	vecaddbyvec(a, b, c, iteration_cnt)
+	vector_time = toq();
+	tic()
+	vecaddbyiter(a, b, c, iteration_cnt)
+	iteration_time = toq();
+	vector_wins[i, j] = iteration_time > vector_time ? true: false
+	if vector_wins[i, j]
+		@printf "For two %d vectors to add for %d times, vector add wins!" current_vector_size iteration_cnt
+	else
+		@printf "For two %d vectors to add for %d times, iteration add wins!" current_vector_size iteration_cnt
+	end
+end
+@printf "%s" a
