b5b9112d8c4d8bcc6a1132a1c853fe3712643dbe
QU Xiaofeng <xiaofeng.qu.hk@ieee.org>
2016-05-08 18:08:18 +0800

    Optimized with macros @inbounds and @simd
    Signed-off-by: QU Xiaofeng <xiaofeng.qu.hk@ieee.org>

--- src/JuliaPerformanceVerification.jl
+++ src/JuliaPerformanceVerification.jl
@@ -5,14 +5,16 @@ module JuliaPerformanceVerification
 export vecaddbyvec!, vecaddbyiter!
 
 function vecaddbyvec!(a, b, c, N)
-	for i = 1:N
-		c[:] = a + b
+	@simd for i = 1:N
+		@inbounds c[:] = a + b
 	end
 end
 
 function vecaddbyiter!(a, b, c, N)
-	for i = 1:N, j = 1:length(a)
-		c[j] = a[j] + b[j]
+	for i = 1:N
+		@simd for j = 1:length(a)
+			@inbounds c[j] = a[j] + b[j]
+		end
 	end
 end
 
