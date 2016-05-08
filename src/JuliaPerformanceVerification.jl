1ee078eaf3396dfd086392c7cdacb714a6327696
QU Xiaofeng <xiaofeng.qu.hk@ieee.org>
2016-05-08 18:27:58 +0800

    Optimized with macros @inbounds and @simd
    Signed-off-by: QU Xiaofeng <xiaofeng.qu.hk@ieee.org>

--- src/JuliaPerformanceVerification.jl
+++ src/JuliaPerformanceVerification.jl
@@ -2,15 +2,27 @@ module JuliaPerformanceVerification
 
 # package code goes here
 
-export vecaddbyvec!, vecaddbyiter!
+export vecaddbyvec!, vecaddbyiter!, vecaddbyvec_oped!, vecaddbyiter_oped!
 
 function vecaddbyvec!(a, b, c, N)
+	for i = 1:N
+		c[:] = a + b
+	end
+end
+
+function vecaddbyiter!(a, b, c, N)
+	for i = 1:N, j = 1:length(a)
+		c[j] = a[j] + b[j]
+	end
+end
+
+function vecaddbyvec_oped!(a, b, c, N)
 	@simd for i = 1:N
 		@inbounds c[:] = a + b
 	end
 end
 
-function vecaddbyiter!(a, b, c, N)
+function vecaddbyiter_oped!(a, b, c, N)
 	for i = 1:N
 		@simd for j = 1:length(a)
 			@inbounds c[j] = a[j] + b[j]
--- test/runtests.jl
+++ test/runtests.jl
@@ -17,6 +17,8 @@ for (i, current_vector_size) in enumerate(vector_size_range), (j, iteration_cnt)
 	b = rand(current_vector_size)
 	c_vec = zeros(current_vector_size)
 	c_iter = zeros(current_vector_size)
+	c_vec_oped = zeros(current_vector_size)
+	c_iter_oped = zeros(current_vector_size)
 	tic()
 	vecaddbyvec!(a, b, c_vec, iteration_cnt)
 	vector_time = toq();
@@ -24,6 +26,15 @@ for (i, current_vector_size) in enumerate(vector_size_range), (j, iteration_cnt)
 	vecaddbyiter!(a, b, c_iter, iteration_cnt)
 	iteration_time = toq();
 	@test c_vec == c_iter
+	tic()
+	vecaddbyvec_oped!(a, b, c_vec_oped, iteration_cnt)
+	vector_oped_time = toq();
+	tic()
+	vecaddbyiter_oped!(a, b, c_iter_oped, iteration_cnt)
+	iteration_oped_time = toq();
+	@test c_vec == c_iter
+	@test c_vec_oped == c_iter
+	@test c_vec == c_iter_oped
 	vector_wins[i, j] = iteration_time > vector_time ? true: false
 	vector_faster[i, j] = iteration_time / vector_time
 	if vector_wins[i, j]
@@ -31,6 +42,7 @@ for (i, current_vector_size) in enumerate(vector_size_range), (j, iteration_cnt)
 	else
 		@printf "Two %d dim vectors adding for %d times, iteration add wins %.03f faster!\n" current_vector_size iteration_cnt 1/vector_faster[i, j]
 	end
+	@printf "  @inbounds and @simd accelerate ratio Vector: %.03f; Iteration: %.03f\n\n" vector_time/vector_oped_time iteration_time/iteration_oped_time
 end
 
 @printf "The vector add wins:\n %s \n" vector_wins
